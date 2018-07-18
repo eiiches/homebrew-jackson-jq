e718d2a0483a729a734f14ae114cff1dbd140421
Eiichi Sato <sato.eiichi@gmail.com>
2018-07-19 03:02:42 +0900

    jackson-jq 0.0.9

--- jackson-jq.rb
+++ jackson-jq.rb
@@ -1,9 +1,9 @@
 class JacksonJq < Formula
   desc "Jackson jq command line JSON processor"
   homepage "https://github.com/eiiches/jackson-jq"
-  version "0.0.7"
-  url "http://central.maven.org/maven2/net/thisptr/jackson-jq-cli/0.0.7/jackson-jq-cli-0.0.7.jar"
-  sha256 "d2d6188370471013a380e5a07c91160537241c8de655946100bf9114d65401f9"
+  version "0.0.9"
+  url "http://central.maven.org/maven2/net/thisptr/jackson-jq-cli/0.0.9/jackson-jq-cli-0.0.9.jar"
+  sha256 "ef6106de52b73fc5e79201a116930f271902615a2bd160978b9f828fdd2ed961"
 
   bottle :unneeded
 
